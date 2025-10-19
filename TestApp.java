a8a2fd06f0c43d7088933748fafe4233241b8d55
Volodymyr Babak <volodimirbabak502@gmail.com>
2025-10-19 23:29:07 +0300

    println div added

--- TestApp.java
+++ TestApp.java
@@ -1,18 +1,9 @@
-// Ваш рядок "package..." може відрізнятися, залиште його як є
 package testapp;
-
-// Цей рядок важливий для зчитування вводу з клавіатури!
-import java.util.Scanner; 
 
 public class TestApp {
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner in = new Scanner(System.in);
-        System.out.print("1st num: ");
-        double a = Double.valueOf(in.nextLine());
-        System.out.print("2nd num: ");
-        double b = Double.valueOf(in.nextLine());
-        System.out.println("a+b="+(a+b));
-        System.out.println("GIT test application");
+        SimpleCalc sc = new SimpleCalc(20, 10);
+        // Змінюємо/додаємо 
+        System.out.println("a/b="+(sc.getDiv()));
     }
 }
