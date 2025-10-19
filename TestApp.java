457f7b2b824dfd757d9101fdcd07974f8036c5ac
Volodymyr Babak <volodimirbabak502@gmail.com>
2025-10-19 18:06:22 +0300

    Додав інтерактивне введення чисел через Scanner

--- TestApp.java
+++ TestApp.java
@@ -1,11 +1,17 @@
 // Ваш рядок "package..." може відрізнятися, залиште його як є
-package testapp; 
+package testapp;
+
+// Цей рядок важливий для зчитування вводу з клавіатури!
+import java.util.Scanner; 
 
 public class TestApp {
     public static void main(String[] args) {
         // TODO code application logic here
-        float a=10;
-        float b=(float) 12.5;
+        Scanner in = new Scanner(System.in);
+        System.out.print("1st num: ");
+        double a = Double.valueOf(in.nextLine());
+        System.out.print("2nd num: ");
+        double b = Double.valueOf(in.nextLine());
         System.out.println("a+b="+(a+b));
         System.out.println("GIT test application");
     }
