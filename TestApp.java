40c0e2a5df459d5b78dfbd7e082cecefb2000764
Volodymyr Babak <volodimirbabak502@gmail.com>
2025-10-19 18:04:59 +0300

    Додав прості обчислення з float

---
+++ TestApp.java
@@ -0,0 +1,12 @@
+// Ваш рядок "package..." може відрізнятися, залиште його як є
+package testapp; 
+
+public class TestApp {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float a=10;
+        float b=(float) 12.5;
+        System.out.println("a+b="+(a+b));
+        System.out.println("GIT test application");
+    }
+}
